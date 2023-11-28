c1c6014ce15826af13bd9f19590921f4297e5624
styyyyyyle <130631756+sandunppt@users.noreply.github.com>
2023-11-28 23:17:09 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
+  title: '三顿翻译助手',
   description: 'App description',
   copyright: '',
   privacy_policy: '',
